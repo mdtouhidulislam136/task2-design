8b55a92e5fcecc9c548825b0d06c914c68789f17
Md Touhidul <mdtouhidul.islam@tuni.fi>
2022-05-19 23:20:35 +0300

    some changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,9 +7,7 @@ export default function Home() {
       <img src="./robin.jpg" className="profile_image" alt="" />
         <h5>You Name</h5>
         <h6>Add title or description here</h6>
-        <div>
-          <h6 className="home"><i class="fa-solid fa-house"></i> Home</h6>
-        </div>
+        <h6 className="home"><i class="fa-solid fa-house"></i> Home</h6>
         <a href="" className="menu">Menu <i class="fa-solid fa-arrow-right"></i></a>
         <h6 > <i class="fa-solid fa-user"> </i> Profile View</h6>
         <h6> <i class="fa-solid fa-gear"></i>Settings View</h6>
